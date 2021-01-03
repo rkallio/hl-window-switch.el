2a502b5be6a667b9ec28d3ae4b5c32a513e88d9a
Roni Kallio <roni@kallio.app>
2021-01-04 01:53:07 +0200

    First revision
    Highlighting implemented using `pulse'.  For now, if you have two
    windows containing the same buffer, both windows will highlight the line
    of the mark.  This is due to Emacs handling overlays on a per-buffer,
    not on a per-window basis.

--- hl-window-switch.el
+++ hl-window-switch.el
@@ -27,5 +27,48 @@
 ;;
 ;;; Code:
 
+(require 'pulse)
+
+(defcustom hl-window-switch-pulse-face 'pulse-highlight-start-face
+  "Face applied to pulse overlay when a buffer is switched to."
+  :type 'face
+  :group 'hl-window-switch)
+(windowp nil)
+
+
+(defun hl-window-switch--highlight-line-of-point (&optional frame-or-window)
+  "Highlight the location  that the point is on in FRAME-OR-WINDOW.
+If FRAME-OR-WINDOW is a frame, pick the current buffer in the
+selected window of that frame.  If FRAME-OR-WINDOW is a window,
+pick the current buffer in that window.  If FRAME-OR-WINDOW is
+omitted or nil, use the frame returned from function
+`selected-frame'.  In case the buffer is a minibuffer, do not
+apply highlighting."
+  (let* ((frame (cond ((framep frame-or-window) frame-or-window)
+                      ((windowp frame-or-window) (window-frame frame-or-window))
+                      ((null frame-or-window) (selected-frame))
+                      (t (signal 'wrong-type-argument frame-or-window))))
+         (window (car (window-list frame 'no-minibuffer nil)))
+         (buffer (window-buffer window)))
+    (unless (minibufferp buffer)
+      (with-current-buffer buffer
+        (pulse-momentary-highlight-one-line (point)
+                                            hl-window-switch-pulse-face)))))
+
+(defvar hl-window-switch--hooks '(window-selection-change-functions
+                                  window-buffer-change-functions)
+  "List of hooks that command `hl-window-switch-mode' applies a function to.")
+
+;;;###autoload
+(define-minor-mode hl-window-switch-mode
+  "Toggle window switch highlighting."
+  :require 'hl-window-switch
+  :global t
+  (if hl-window-switch-mode
+      (dolist (hook hl-window-switch--hooks)
+        (add-hook hook #'hl-window-switch--highlight-line-of-point))
+    (dolist (hook hl-window-switch--hooks)
+      (remove-hook hook #'hl-window-switch--highlight-line-of-point))))
+
 (provide 'hl-window-switch)
 ;;; hl-window-switch.el ends here
