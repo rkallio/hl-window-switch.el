7b07eccb0f60ba00ed5265223cd5eb0b3a1c9667
Roni Kallio <roni@kallio.app>
2021-01-04 01:50:34 +0200

    Package definition

---
+++ hl-window-switch.el
@@ -0,0 +1,31 @@
+;;; hl-window-switch.el --- Highlight point when switching windows -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Roni Kallio
+
+;; Author: Roni Kallio <roni@kallio.app>
+;; Created: 4 Jan 2021
+;; Keywords: frames
+;; URL: https://github.com/rkallio/hl-window-switch
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; The license text: <http://www.gnu.org/licenses/gpl-3.0.html>
+
+;;; Commentary:
+;;
+;; Highlight point when switching windows.
+;;
+;;; Code:
+
+(provide 'hl-window-switch)
+;;; hl-window-switch.el ends here
